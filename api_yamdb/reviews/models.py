80b1d6a0666afb933e3b9d2072ac2bc730934454
Ксения Малютина <eduvxn@ya.ru>
2022-06-22 21:52:55 +0300

    first commit, structure of develop branch added

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class User(models.Model):
+    pass
+
+
+class Review(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class Genre(models.Model):
+    pass
+
+
+class Title(models.Model):
+    pass
