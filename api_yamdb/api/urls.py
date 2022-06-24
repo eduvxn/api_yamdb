e906ca74c7e50331762d73c5ff7ea02509537468
Елена Еловцева <elovcev@rambler.ru>
2022-06-24 11:47:23 +0300

    Добавлены модели,сериализаторы,вьюÑшкиÐÐ

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,3 +1,20 @@
+from rest_framework.routers import SimpleRouter
+
 from django.urls import include, path
 
-# add your urls here
+from .views import CommentViewSet, CommentsViewSet, ReviewViewSet
+
+app_name = 'api'
+
+router = SimpleRouter()
+
+router.register(r'titles/(?P<title_id>\d+)/reviews',
+                ReviewViewSet, basename='reviews')
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentsViewSet, basename='comments'
+)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
