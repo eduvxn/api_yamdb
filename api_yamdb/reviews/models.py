385dc17d5605b6c29c06db30f721e6972e98e77b
Елена Еловцева <elovcev@rambler.ru>
2022-06-27 15:22:47 +0300

    исправлены ошибки pytest

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -112,7 +112,8 @@ class Review(models.Model):
         related_name='reviews',
         verbose_name='Автор')
     title = models.ForeignKey(
-        Title, on_delete=models.CASCADE)
+        Title, on_delete=models.CASCADE,
+        related_name='reviews')
     text = models.TextField()
     score = models.IntegerField(validators=[MinValueValidator(1),
                                             MaxValueValidator(10)])
